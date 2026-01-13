715740f1dd75a5d0a44f4868d7919249568f094e
csdivyansh <idivyanshv@gmail.com>
2026-01-13 14:47:09 +0530

    added more endpoints

--- Day1/src/main/java/com/example/Day1/Hello.java
+++ Day1/src/main/java/com/example/Day1/Hello.java
@@ -9,4 +9,14 @@ public class Hello {
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/hello")
+    public String first() {
+        return "This is the first endpoint!";
+    }
+    
+    @GetMapping("/about")
+    public String about() {
+        return "this is the about page.";
+    }
 }
