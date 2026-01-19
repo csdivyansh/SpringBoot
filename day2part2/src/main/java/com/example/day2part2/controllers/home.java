e5541bcbb184d77ffc568d736f9f320285b85286
csdivyansh <idivyanshv@gmail.com>
2026-01-19 15:17:09 +0530

    Added 5 students

--- day2part2/src/main/java/com/example/day2part2/controllers/home.java
+++ day2part2/src/main/java/com/example/day2part2/controllers/home.java
@@ -1,6 +1,8 @@
 package com.example.day2part2.controllers;
 
-import com.example.day2part2.*;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,13 @@ import com.example.day2part2.models.StudentModel;
 @RestController
 public class home {
     @GetMapping("/")
-    public StudentModel about(){
-        StudentModel student = new StudentModel(1, "Divyansh", "idivyanshv@gmail.com");
-        return student;
+    public List<StudentModel> about(){
+        StudentModel student1 = new StudentModel(1, "Divyansh", "idivyanshv@gmail.com");
+        StudentModel student2 = new StudentModel(2, "Aman", "aman@gmail.com");
+        StudentModel student3 = new StudentModel(3, "Riya", "riya@gmail.com");
+        StudentModel student4 = new StudentModel(4, "Kunal", "kunal@gmail.com");
+        StudentModel student5 = new StudentModel(5, "Sneha", "sneha@gmail.com");
+
+        return Arrays.asList(student1, student2, student3, student4, student5);
     }
 }
